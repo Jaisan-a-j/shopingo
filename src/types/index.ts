731fd337e99364f396de4e004a12701b4441aa0f
jaisan <jaisanaj1999@gmail.com>
2026-05-01 11:21:36 +0200

    fixed missing product type

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,3 +5,12 @@ export interface CategoryItem {
   label: string;
   Icon: ElementType;
 }
+
+export interface Product {
+  _id: string;
+  name: string;
+  category: string;
+  price: number;
+  rating: number;
+  image: string;
+}
